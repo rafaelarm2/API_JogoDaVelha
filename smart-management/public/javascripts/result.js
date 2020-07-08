ca345e052d033d852e40af48aa1d7b4e4be97a32
Rafaela <moreiramrafaela@gmail.com>
2020-07-08 19:35:52 -0300

    Função result verificando se houve velha e alguma vitória

--- smart-management/public/javascripts/result.js
+++ smart-management/public/javascripts/result.js
@@ -1,36 +1,50 @@
-const row = 2;
-const col = 2;
-
+const dim_matrix = 2;
 
 function result(movements, id) {
-  var tableGame = new Array(2);
-
-  for (var i = 0; i < x.length; i++) {
-    tableGame[i] = new Array(2);
-  }
+  var draw, victoryX, victoryO = false;
+  var colX, rowX, colO, rowO = [0,0,0];
+  var dia1X, dia1O, dia2X, dia2O, contMovement = 0;
 
   for(let i = 0; i < movements.length; i++){
     if (movements[i].id == id) {
-      movementsOfAGame[movements[i].position.x][movements[i].position.y] = movements[i].player;
+      contMovement++;
+      if (movements.player == 'X') {
+        colX[movements[i].position.x]++;
+        rowX[movements[i].position.y]++;
+
+        if (movements[i].position.x == movements[i].position.y) {
+          dia2X++;
+        }
+
+        if ((movements[i].position.x + movements[i].position.y) == dim_matrix) {
+          dia1X++;
+        }
+
+      } else {
+        colO[movements[i].position.x]++;
+        rowO[movements[i].position.y]++;
+
+        if (movements[i].position.x == movements[i].position.y) {
+          dia2O++;
+        }
+
+        if ((movements[i].position.x + movements[i].position.y) == dim_matrix) {
+          dia1O++;
+        }
+      }
+    }
+
+    if (colO[0] == 3 || colO[1] == 3 || colO[2] == 3 || rowO[0] == 3 || rowO[1] == 3 || rowO[2] == 3 || dia1O == 3 || dia2O == 3) {
+      victoryO = false;
+    }
+
+    if (colX[0] == 3 || colX[1] == 3 || colX[2] == 3 || rowX[0] == 3 || rowX[1] == 3 || rowX[2] == 3 || dia1X == 3 || dia2X == 3) {
+      victoryO = false;
+    }
+
+    if (contMovement == 9 && !victoryO && !victoryX) {
+      draw == true;
     }
   }
-
-  checkCol(movementsOfAGame);
-}
-
-function checkCol(movements) {
-
-}
-
-function checkRow(movements) {
-
-}
-
-function checkDiagonal1(movements) {
-
-}
-
-
-function checkDiagonal2(movements) {
 
 }
